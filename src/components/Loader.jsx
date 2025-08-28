caa076e610ab1f25a1020c33e4f10ca572c48d0a
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-08-28 10:28:53 +0530

    loader compoenent added

---
+++ src/components/Loader.jsx
@@ -0,0 +1,8 @@
+
+const Loader = () => {
+  return (
+    <div className="w-6 h-6 border-4 border-white border-t-transparent rounded-full animate-spin mx-auto"></div>
+  );
+};
+
+export default Loader;
